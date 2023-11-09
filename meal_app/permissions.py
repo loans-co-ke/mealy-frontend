c892d4e26dbc2fbc78d8de05498c11d2700da196
BrianOdhiambo <brianodhiambo530@gmail.com>
2023-11-09 23:07:24 +0300

    Added permission file

---
+++ meal_app/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+class IsSysAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_admin)
+    
+class IsCustomer(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_customer)
